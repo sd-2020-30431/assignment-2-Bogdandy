9b5ffdcdc24499f889c4111c6dc21031c5f6262a
Dragoteanu Bogdan <45183646+Bogdandy@users.noreply.github.com>
2020-04-17 14:18:42 +0300

    Delete AbstractFactory.java

--- server/business/AbstractFactory.java
+++
@@ -1,8 +0,0 @@
-package server.business;
-
-import server.dataaccess.GroceryItem;
-import java.util.List;
-
-public interface AbstractFactory<T> {
-    T create(String type, List<GroceryItem> groceryItem);
-}
