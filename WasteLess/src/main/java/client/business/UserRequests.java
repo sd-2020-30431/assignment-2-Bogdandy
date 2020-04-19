7e85dc151f284693abe3eff0e4733578acaf1576
Dragoteanu Bogdan <45183646+Bogdandy@users.noreply.github.com>
2020-04-19 20:08:07 +0300

    Add files via upload

---
+++ WasteLess/src/main/java/client/business/UserRequests.java
@@ -0,0 +1,10 @@
+package client.business;
+
+public class UserRequests {
+    public String LogInRequest(){
+        return null;
+    }
+    public String SignUpRequest(){
+        return null;
+    }
+}
