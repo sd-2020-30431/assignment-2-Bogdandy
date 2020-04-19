b17cbe35dc1d2e44158471a22544ffa55fe4a36e
Dragoteanu Bogdan <45183646+Bogdandy@users.noreply.github.com>
2020-04-19 20:01:01 +0300

    Delete UserRepository.java

--- java/com/boot/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.boot.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.boot.entities.*;
-
-public interface UserRepository extends CrudRepository<User, Long>{
-
-}
